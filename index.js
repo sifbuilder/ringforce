890fba4cff413a64fe41a16a346508af481f9dd6
sifbuilder <sifbuilder@gmail.com>
2016-06-16 00:59:05 +0200

    data

--- index.js
+++ index.js
@@ -10,20 +10,71 @@ var simulation = d3.forceSimulation()
     .force("charge", d3.forceManyBody())
     .force("center", d3.forceCenter(width / 2, height / 2));
 
+		
 
-d3.json("data.json", function(error, graph) {
-  if (error) throw error;
+var circleData = [];
+var linkData = [];
+
+// Define first circle and type of motion:- circular
+var circleData = [{
+	id: 1,
+	group: 1,
+	cr: 10,
+	rotr: 100,
+	rtype: "circle",
+	offset: 20,
+	timelimit: 2000,
+	starttime: undefined,
+	elapsed: 0,
+	x: 100,
+	y: 100,
+	scale: 1,
+	move: false,
+}, {
+	id: 2,
+	group: 1,
+}, {
+	id: 3,
+	group: 1,
+}, {
+	id: 4,
+	group: 1,
+}]
+
+
+var linkData = [{
+	source: 1,
+	target: 2,
+	index: 1,
+}, {
+	source: 2,
+	target: 1,
+	index: 2,
+}, {
+	source: 2,
+	target: 3,
+	index: 3,
+}]
+
+console.log(JSON.stringify(circleData, null, 2))
+console.log(JSON.stringify(linkData, null, 2))
+
+
+// d3.json("data.json", function(error, graph) {
+  // if (error) throw error;
 
   var link = svg.append("g")
       .attr("class", "links")
     .selectAll("line")
-    .data(graph.links)
+    .data(linkData)
+    // .data(graph.links)
     .enter().append("line");
 
   var node = svg.append("g")
       .attr("class", "nodes")
     .selectAll("circle")
-    .data(graph.nodes, function(d) { return d.id })
+    .data(circleData, function(d) { return d.id })
+    // .data(graph.nodes, function(d) { return d.id })
     .enter().append("circle")
       .attr("r", 6.5)
       .call(d3.drag()
@@ -35,11 +86,15 @@ d3.json("data.json", function(error, graph) {
       .text(function(d) { return d.id; });
 
   simulation
-      .nodes(graph.nodes)
+      .nodes(circleData)
+      // .nodes(graph.nodes)
       .on("tick", ticked);
 
   simulation.force("link")
-      .links(graph.links);		//
+      .links(linkData);		//
+      // .links(graph.links);		//
+
+			
 
   function ticked() {
     link
@@ -52,7 +107,7 @@ d3.json("data.json", function(error, graph) {
         .attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
   }
-});
+// });
 
 function dragstarted(d) {
   if (!d3.event.active) simulation.alphaTarget(0.3).restart()
